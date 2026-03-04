be829d64a7e326e0400f504b2f8d52b5b9a9e2f2
Leo Solorzano <lrsolorzano@gmail.com>
2026-03-04 10:18:44 -0600

    Add Terraform variable definitions for GCP configuration

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,21 +1,41 @@
-# Terraform Variables
+// Terraform variables for GCP configuration
 
 variable "project_id" {
-  description = "The GCP project ID"
+  description = "The GCP project ID."
   type        = string
 }
 
 variable "region" {
-  description = "The GCP region"
+  description = "The region for the GCP resources."
   type        = string
 }
 
-variable "instance_types" {
-  description = "List of GCP instance types"
-  type        = list(string)
+variable "zone" {
+  description = "The zone for the GCP resources."
+  type        = string
 }
 
-variable "network_configuration" {
-  description = "Network configuration for the GCP instance"
-  type        = map(string)
+variable "network_name" {
+  description = "The name of the GCP network."
+  type        = string
+}
+
+variable "subnetwork_name" {
+  description = "The name of the GCP subnetwork."
+  type        = string
+}
+
+variable "instance_type" {
+  description = "The instance type for GCP VM instances."
+  type        = string
+}
+
+variable "machine_image" {
+  description = "The machine image to use for instances."
+  type        = string
+}
+
+variable "service_account_email" {
+  description = "The email of the service account."
+  type        = string
 }
