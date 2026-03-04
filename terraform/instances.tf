cccaf329b398b7d1a12e4a5c621e3d7eb3d7b4ad
Leo Solorzano <lrsolorzano@gmail.com>
2026-03-04 10:17:23 -0600

    Add Terraform configuration for GCE instances

--- terraform/instances.tf
+++ terraform/instances.tf
@@ -1,16 +1,68 @@
-# Instances Configuration for Terraform
-
-resource "aws_instance" "kali_linux" {
-  ami           = "ami-0abcdef1234567890" # Replace with the correct AMI ID for Kali Linux
-  instance_type = "t2.micro"
+provider "google" {
+  credentials = file("path_to_your_credentials.json")
+  project     = "your_project_id"
+  region      = "us-central1"
 }
 
-resource "aws_instance" "ubuntu_22_04" {
-  ami           = "ami-0abcdef1234567890" # Replace with the correct AMI ID for Ubuntu 22.04
-  instance_type = "t2.micro"
+resource "google_compute_address" "kali_ip" {
+  name = "kali-static-ip"
 }
 
-resource "aws_instance" "windows_server_2022" {
-  ami           = "ami-0abcdef1234567890" # Replace with the correct AMI ID for Windows Server 2022
-  instance_type = "t2.micro"
+resource "google_compute_instance" "kali" {
+  name         = "kali-linux"
+  machine_type = "e2-standard-2"
+  zone         = "us-central1-a"
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/kali-linux"
+    }
+  }
+  network_interface {
+    network = "default"
+    access_config {
+      nat_ip = google_compute_address.kali_ip.address
+    }
+  }
+}
+
+resource "google_compute_address" "ubuntu_ip" {
+  name = "ubuntu-static-ip"
+}
+
+resource "google_compute_instance" "ubuntu" {
+  name         = "ubuntu-server"
+  machine_type = "e2-standard-2"
+  zone         = "us-central1-a"
+  boot_disk {
+    initialize_params {
+      image = "ubuntu-os-cloud/ubuntu-2204-lts"
+    }
+  }
+  network_interface {
+    network = "default"
+    access_config {
+      nat_ip = google_compute_address.ubuntu_ip.address
+    }
+  }
+}
+
+resource "google_compute_address" "windows_ip" {
+  name = "windows-static-ip"
+}
+
+resource "google_compute_instance" "windows" {
+  name         = "windows-server"
+  machine_type = "e2-standard-4"
+  zone         = "us-central1-a"
+  boot_disk {
+    initialize_params {
+      image = "windows-cloud/windows-server-2022-dc"
+    }
+  }
+  network_interface {
+    network = "default"
+    access_config {
+      nat_ip = google_compute_address.windows_ip.address
+    }
+  }
 }
